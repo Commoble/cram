7afa7f5c1c80c67e79a60f3a75d8005b792f0016
Commoble <josephbettendorff@yahoo.com>
2021-01-21 14:00:40 -0600

    Small tweak to blockitem helper

--- src/main/java/net/minecraft/item/CramBlockItemHelper.java
+++ src/main/java/net/minecraft/item/CramBlockItemHelper.java
@@ -1,16 +1,15 @@
 package net.minecraft.item;
 
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 
 /** Cheap trick for accessing protected methods in BlockItem **/
-public abstract class CramBlockItemHelper extends BlockItem
+public abstract class CramBlockItemHelper
 {
 
-	public CramBlockItemHelper(Block blockIn, Properties builder)
-	{
-		super(blockIn, builder);
-	}
+//	public CramBlockItemHelper(Block blockIn, Properties builder)
+//	{
+//		super(blockIn, builder);
+//	}
 
 	public static BlockState getStateForPlacement(BlockItem blockItem, BlockItemUseContext context)
 	{
